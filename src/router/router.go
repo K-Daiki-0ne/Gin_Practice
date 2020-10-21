1a0006f79b2a54a818950c4b41e4c8b91500a362
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-10-22 00:00:07 +0900

    add: routing parameter

--- src/router/router.go
+++ src/router/router.go
@@ -10,6 +10,6 @@ import (
 func Router(app *gin.Engine) {
 	app.GET("/api/v1/books", controllers.GetAllBooks)
 	app.GET("/api/v1/book/:id", controllers.GetBook)
-	app.POST("/api/v1/new/book", controllers.NewBook)
+	app.POST("/api/v1/new/book/:title/:author/:rating", controllers.NewBook)
 	app.DELETE("/api/v1/delete/book/:id", controllers.DeleteBook)
 }
